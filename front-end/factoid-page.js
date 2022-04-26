fd8004682ddc5d1042bf2ba717dc4c1b3812de42
Joshua-B <joshua_bloom96@yahoo.co.uk>
2022-04-26 12:22:55 +0100

    added find by id factoid, still WIP

--- front-end/factoid-page.js
+++ front-end/factoid-page.js
@@ -1,7 +1,5 @@
 const apiPath = "http://localhost:8080/"
 
-let currentFactoid = [];
-
 // POST REQUESTS  
 const createFactoid = () => {
     axios.post(apiPath + "factoid/create/", {
@@ -15,5 +13,37 @@ const createFactoid = () => {
         .catch(err => console.error(err));
 };
 
+
+const appendFactoid = async () => {
+    let id = document.getElementById("factoidId").value
+    let factoid = await getFactoidByIdProm(id);
+    let div = document.createElement('div');
+    console.log(factoid);
+    div.setAttribute('class', 'factoid block bc2')
+    div.innerHTML = `<div class="card">
+                        <div class="card-header">${id}</div>
+                        <div class="card-body">
+                            <h5 class="card-title">${factoid.content}</h5>
+                            <p class="card-text">${factoid.explanation}</p>
+                            <p class="card-text">${factoid.axiom}</p>
+                        </div>
+                    </div>`
+    document.getElementById('factoids').appendChild(div);
+    // (factoid => showOutput(factoid));
+}
+
+//GET REQUESTS
+const getFactoidByIdProm = (id) => {
+    return axios.get(apiPath + "factoid/getById/" + id, {})
+    .then(factoid => factoid.data).catch(err => console.error(err));
+};
+
+const showOutput = (factoid) => {
+    document.getElementById('factoid').innerHTML = factoid.data.content;
+};
+
+//TODO:close button resets form, lookup security risk of form input
 const explElement = document.getElementById("createFactoidExplanation");
+
 document.getElementById("createFactoid").addEventListener("click", createFactoid);
+document.getElementById("submitBtn").addEventListener("click", appendFactoid);
